bb7e970eb16103cb27e461ee049d5153fd80644f
Penny Tague <pmtague9@gmail.com>
2019-07-31 13:11:29 -0700

    Added event handlers in sign-up forms

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import 'typeface-roboto';
+import Navigation from './components/Navigation';
 import LoginForm from './components/LoginForm';
 import SignUpForm from './components/SignUpForm';
-import Navigation from './components/Navigation';
-import 'typeface-roboto';
+import ElectionSearch from './components/ElectionSearch';
 
 function App() {
+
+  const [user, setUser] = useState({ username: '', email: '', password: '' });
+
+  const [team, setTeam] = useState([{ username: 'Penny', email: 'pmtague9@gmail.com', password: 'admin' }]);
   return (
     <div className="App">
       <Navigation />
       <LoginForm />
-      <SignUpForm />
+      <SignUpForm team={ team } setTeam={ setTeam } user={ user } setUser={ setUser }/>
+      <ElectionSearch />
     </div>
   );
 }
---
+++ front-end/src/components/ElectionSearch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ElectionSearch() {
+    return (
+        <div>
+            <p>Find Me Some Elections</p>
+        </div>
+    )
+}
